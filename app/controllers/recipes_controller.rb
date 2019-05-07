65442bf734e7068d959702c6fba749a9996bab7c
Marco Tatone <m.tatone@hotmail.com>
2019-05-06 22:02:36 -0400

    added index view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,22 @@
 class RecipesController < ApplicationController
   def index
   end
+
+  def show
+    find_recipe
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+  end
+
+
+  private 
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 end
