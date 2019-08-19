9110560273ca8ebd5e0a822050da397cbff32974
Marco Tatone <m.tatone@hotmail.com>
2019-08-18 23:46:48 -0400

    added users but needs debugging and have to hide and show buttons when users are logged in. also create the relationship between users and recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def index
     console
